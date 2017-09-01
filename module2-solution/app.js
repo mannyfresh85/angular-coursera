050d28f8fae80a3da3c7cbef7a566eb7c54824c5
mannyfresh85 <mannyr.martinez@gmail.com>
2017-09-01 17:13:31 -0400

    module2 intial

---
+++ module2-solution/app.js
@@ -0,0 +1,12 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckoff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController )
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+
+
+
+})();
