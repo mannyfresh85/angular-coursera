bd1856c32d9006dee8efc0b365cc38ade437c9c3
mannyfresh85 <mannyr.martinez@gmail.com>
2017-09-11 23:22:04 -0400

    some progress made

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -11,9 +11,11 @@ NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController( MenuSearchService ) {
   var narrow = this;
 
-  narrow.foundItems = MenuSearchService.getMatchedMenuItems();
+  narrow.findMatches = function () {
+    narrow.found = MenuSearchService.getMatchedMenuItems(narrow.searchTerm);
+    console.log ("Found Items Controller " + narrow.found);
+  }
 
-  console.log ("Found Items" + narrow.foundItems);
 
 }; //end of NarrowItDownController
 
@@ -21,67 +23,30 @@ MenuSearchService.$inject = ['$http', 'ApiBasePath']
 function MenuSearchService( $http, ApiBasePath ) {
   var service = this;
 
-  service.getMatchedMenuItems = function() {
-    return $http({method: "GET", url: (ApiBasePath + "/menu_items.json")})
-    .then(function (result) {
+
+  service.getMatchedMenuItems = function(searchTerm) {
+    //searchTerm = 'garlic';
+    console.log('Search term ' + searchTerm);
+    return $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")
+    }).then(function (result) {
       // process result and only keep items that match
-      var foundItems = []
-      console.log("Result" + result.data['menu_items'][0]);
+      var foundItems = [];
+      //iterate through the menu_items
+      angular.forEach(result.data['menu_items'], function(item, index) {
+        //check description for searchTerm
+        var description = item.description;
+        if ( description.includes(searchTerm)){
+          foundItems.push(item);
+          console.log('Found term ' + item.description)
+        }
+      });
 
-
-      // return processed items
+    // return processed items
       return foundItems;
     });
   }
-
-
-
-
-
-
-  // service.getMenuItems = function () {
-  //     var response = $http({
-  //       method: "GET",
-  //       url: (ApiBasePath + "/menu_items.json")
-  //     });
-  //
-  //     return response;
-  //   };
-
-
-
-  //  service.getMatchedMenuItems = function(searchTerm) {
-  //   var menu = service.getMenuItems();
-
-
-
-  //   menu.then(function (response) {
-  //     service.items = response.data;
-  //     console.log('service items within promise' + service.items['menu_items'])
-  //   })
-  //   .catch(function (error) {
-  //     console.log("Something went terribly wrong.");
-  //   });
-  //
-  //
-  //
-  //   service.foundItems = [];
-  //     console.log(menu['menu_items']);
-  //       //iterate through the menu_items
-  //       angular.forEach(menu['menu_items'], function(item, index) {
-  //         //check description for searchTerm
-  //         var description = item.description;
-  //         if ( description.includes(searchTerm)){
-  //           service.foundItems.push(item);
-  //         }
-  //       });
-  //
-  //       return service.foundItems;
-  //
-  //
-  //   // console.log('Down Here ' + service.items );
-  //   // return searchTerm;
-  // }; //end of getMatchedMenuItems
 
 }; //end of MenuSearchService
 
